d5795ce85262e7f61b5563b0fa041f5abfcc92e3
Duc Thanh Nguyen <92532391+ThanhND-1501@users.noreply.github.com>
2023-08-12 16:24:00 +0700

    change the download link to my drive

--- tool/utils.py
+++ tool/utils.py
@@ -17,13 +17,13 @@ def download_weights(id_or_url, cached=None, md5=None, quiet=False):
 
 weight_url = {
     "pan_resnet18_default": "1GKs-NnezTc6WN0P_Zw7h6wYzRRZdJFKl" ,
-    "pan_resnet18_sroie19": "1-QvIN0MrP28URQILYvLaF1G1eTx2oh8W" ,
-    "transformerocr_mcocr": "1qpXp_-digz2HPTGY_GPdwstzGLhjC_ot",
+    "pan_resnet18_sroie19": "13uV1od6JYhx8e7wPnMJ8l-TG3AtqvY-F" ,
+    "transformerocr_mcocr": "1CXPNdjlSMGNkW_nVAtcdCxDBDxa_E5Pj",
     "transformerocr_default_vgg": "13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA",
     "transformerocr_default_resnet50": "12dTOZ9VP7ZVzwQgVvqBWz5JO5RXXW5NY",
     "transformerocr_default_resnet50_fpn": "12dTOZ9VP7ZVzwQgVvqBWz5JO5RXXW5NY",
     "transformerocr_config": "1xQqR9swWNCTLEa0ensPDT0HDBHTke3xT",
-    "phobert_mcocr": "1v4GQPg4Jx5FWvqJ-2k9YCxEd6iFdlXXa"
+    "phobert_mcocr": "1kCqIM_N3LSrOqXjpd78LZxpcUvVvlM6u"
 }
 
 def download_pretrained_weights(name, cached=None):
